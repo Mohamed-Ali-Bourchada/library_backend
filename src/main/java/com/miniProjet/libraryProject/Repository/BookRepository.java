3fbd81908fcbadc62a34ecf6302f337c5d98abf3
Amine <elbennaedu200@gmail.com>
2024-12-02 17:38:42 +0100

    addRecherche

--- src/main/java/com/miniProjet/libraryProject/Repository/BookRepository.java
+++ src/main/java/com/miniProjet/libraryProject/Repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.miniProjet.libraryProject.Repository;
 
 import com.miniProjet.libraryProject.Entity.Book;
+import com.miniProjet.libraryProject.Enumes.Category;
+import com.miniProjet.libraryProject.Enumes.StateBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,7 +17,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Override
     void deleteById(Long id);
 
-    Book findByTitle(String title);
+    //Book findByTitle(String title);
     boolean existsByAuthorAndTitle(String author, String title);
 
+    List<Book> findByCategory(Category category);
+
+    List<Book> findByStateBook(StateBook stateBook);
+
+    List<Book> findByTitle(String title);
 }
